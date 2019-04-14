edd58ba98920e24ce2a2a48cfd3c5991e718754f
terrysducks <41840392+terrysducks@users.noreply.github.com>
2019-04-14 12:27:09 -0400

    hey

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO_URI, {
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    introspection: true,
+    playgroundd: true,
     context: async ({ req }) => {
         let authToken = null
         let currentUser = null
